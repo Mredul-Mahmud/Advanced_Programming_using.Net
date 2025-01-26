523805eaf55feab46c1b225abcba2ef2d7da5efe
Mredul Mahmud <123942193+Mredul-Mahmud@users.noreply.github.com>
2025-01-27 02:50:13 +0600

    commit

---
+++ aspBackendFinalDemo/DAL/Migrations/202501141744398_Demoinit.cs
@@ -0,0 +1,42 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Demoinit : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CatId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CatId, cascadeDelete: true)
+                .Index(t => t.CatId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "CatId", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CatId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
